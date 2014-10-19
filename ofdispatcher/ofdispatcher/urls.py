890f04a0c15b2270e5a387a4a8779b7dc7c9051e
Manuel Peuster <manuel@peuster.de>
2014-10-19 11:39:35 +0200

    First UI.

--- ofdispatcher/ofdispatcher/urls.py
+++ ofdispatcher/ofdispatcher/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ofdispatcher.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+   url(r'^dispatcher/', include('dispatcher.urls', namespace='dispatcher')),
+   url(r'^admin/', include(admin.site.urls)),
 )
